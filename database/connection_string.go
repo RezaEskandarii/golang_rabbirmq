870463d18e88b9b29df9a738fc93bbccb21cf871
rezaesskandari@gmail.com <mazandarani123456##>
2020-06-04 20:37:59 +0430

    feat: create db connection string in database module

---
+++ database/connection_string.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// return database connection string per DB_ENGINE property in .env file or OS environment variables
+func getConnectionString(dbEngine string) string {
+
+	switch strings.TrimSpace(dbEngine) {
+
+	case "postgres":
+		return fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"),
+		)
+
+	case "mysql":
+		return fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4,utf8&parseTime=True",
+			os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"), os.Getenv("DB_NAME"),
+		)
+
+	case "mssql":
+		return fmt.Sprintf("sqlserver://%s:%s@%s:%s?database=%s",
+			os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"),
+		)
+
+	default:
+		// return postgresql connection string
+		return fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"),
+		)
+
+	}
+}
